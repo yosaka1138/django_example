aeff31d8c4540e4eb66ad803c9aa218646161c2f
yosaka1138 <fallingfluit@gmail.com>
2022-03-01 19:47:25 +0900

    modify secret key

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+try:
+    from .local_settings import *
+except:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-oml%zj1@4l#bt5iy1c!0)zb!&e$neeq53be7$8nklzsc5%w^e$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
